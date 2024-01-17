236576532d0d6d6a60a0ba7a7927a13322d366b5
mkrzyzan <marcin.adam.k@gmail.com>
2024-01-17 23:18:56 +0800

    creating ecosystem script

--- scripts/initEcosystem.js
+++ scripts/initEcosystem.js
@@ -7,19 +7,27 @@ async function init() {
         const provider = new ethers.providers.Web3Provider(web3Provider);
         const signer = provider.getSigner(0)
         const factory = new ethers.ContractFactory(metadata.abi, metadata.data.bytecode.object, signer)
+        const mainAddress = await signer.getAddress();
 
         const contract = await factory.deploy()   
         await contract.deployed()
 
         // mint 4 tokens, and give them to Olive (0x7BA3E64b8Da538AbB7C3Adc72002A2fAF3657d77)
         const imageUrl = 'https://nowszawersja.pages.dev/images?file=8888';
-        const txs = [];
+
+        txs = [];
         for (i = 0; i < 4; i++) {
             nftData = `37.${i}g;99.${i}%;MTS Premium Bullion;MTS;MTS Gold Co., Ltd.`;
             tx = await contract.safeMint(i, nftData, imageUrl);
-            tx2 = await contract.transferFrom(await signer.getAddress(), '0x7BA3E64b8Da538AbB7C3Adc72002A2fAF3657d77', i);
             txs.push(tx.wait());
-            txs.push(tx2.wait());
+        }
+        Promise.all(txs);
+
+        txs = [];
+        for (i = 0; i < 4; i++) {
+            nftData = `37.${i}g;99.${i}%;MTS Premium Bullion;MTS;MTS Gold Co., Ltd.`;
+            tx = await contract.transferFrom(mainAddress, '0x7BA3E64b8Da538AbB7C3Adc72002A2fAF3657d77', i);
+            txs.push(tx.wait());
         }
         Promise.all(txs);
 
